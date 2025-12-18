c71642f3ed8247f5486c89ba3e6ceb3e59d24b38
Big N1 <nikolaleshtov10@gmail.com>
2025-12-18 13:05:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 st.title("Моето първо Streamlit приложение")
 name = st.text_input("Как се казваш?")
+age = st.text_input("На колко години си?")
 if name:
    st.write(f"Здравей, {name}!")
+if age:
+   st.write(f"Здравей, {age}!")
